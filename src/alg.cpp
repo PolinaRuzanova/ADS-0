9b09f58e2fbbed9147a8e98fee2ad35d4ed3d56f
PolinaRuzanova <94633834+PolinaRuzanova@users.noreply.github.com>
2022-02-24 02:04:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,9 +2,12 @@
 
 int gcd(int a, int b) {
     // поместить сюда текст реализации алгоритма (на С++)
+    int c = 0;
     for (int i = a; i > 0; i--) {
         if (a%i == 0 && b%i == 0) {
+            c = i;
             break;       
         }      
     }
+    return c;
 }
